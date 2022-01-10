1e6c0c19d4219a15732e0660d2c7e1e658e31281
chanane youssef <sarbone21@gmail.com>
2022-01-10 14:57:38 +0100

    HomePage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Examen_ASP.Net.Data;
+using Examen_ASP.Net.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,25 +8,40 @@ using System.Web.Mvc;
 
 namespace Examen_ASP.Net.Controllers
 {
+
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        // GET: Home
+        private Examen_ASPNetContext db = new Examen_ASPNetContext();
+
+        public ActionResult Index(string search)
         {
-            return View();
+            ViewModel model = new ViewModel();
+            model.BestOffers = db.Products.OrderByDescending(product => product.Price-product.Discount).Take(3);
+            if (!String.IsNullOrEmpty(search))
+            {
+                model.Products = db.Products.Where(s => s.Title.Contains(search));
+            }
+            else
+            {
+                model.Products = db.Products.ToList();
+            }
+            model.Categories = db.Categories.ToList();
+            
+
+            return View(model);
+        }
+        public ActionResult CategorieProducts(int? id)
+        {
+            ViewModel model = new ViewModel();
+            model.Products = db.Products.Where(product=>product.Category_id==id).ToList();
+            Category category = db.Categories.Find(id);
+            ViewBag.category = category.Name;
+            //model.Products = db.Products.ToList();
+
+
+            return View(model);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ Models/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen_ASP.Net.Models
+{
+    public class ViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Product> BestOffers { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        
+    }
+}
